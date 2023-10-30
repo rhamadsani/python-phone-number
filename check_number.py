eb20ee925d2fa877346e876c9b73d755984b0746
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2023-10-30 12:59:04 +0700

    update for code area print

--- check_number.py
+++ check_number.py
@@ -9,17 +9,32 @@ def search_value (value, data) :
     
     return return_data
 
+#start program
+getinput = input("Please Input Phone Number With International Code : ")
+
 #read json list international code 
 try: 
     ic = open("./international_code.json", 'r')
 except FileNotFoundError : 
-    print("File Not Found, Correction Path File Pleae")
+    print("File Not Found, Correction Path File Please")
 
 except FileExistsError : 
-    print("File does'nt Exist, Correction Path File Pleae")
+    print("File does'nt Exist, Correction Path File Please")
 
 content = ic.read()
 
 jsonf = list(json.loads(content))
 
-print(search_value("+62", jsonf))
+#check available internatioanl code
+for2digit = search_value(getinput[0:3], jsonf)
+for3digit = search_value(getinput[0:4], jsonf)
+for4digit = search_value(getinput[0:5], jsonf)
+
+# check result
+result = []
+if (len(for2digit) + len(for3digit) + len(for4digit)) == 0:
+    print("International Code Not Found")
+else : 
+    result = for2digit + for3digit + for4digit
+
+print(result)
